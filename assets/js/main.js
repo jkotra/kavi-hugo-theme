d70071df29b7a84821b12d0cf13924497b91afe1
Jagadeesh Kotra <jagadeesh@stdin.top>
2022-02-07 10:45:00 +0530

    theme updates
    - update normalize.css
    - dynamically change comments theme (light/dark)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+function setCommentsTheme(match){
+    console.log(match)
+    const message = {
+        type: 'set-theme',
+        theme: match ? "github-dark" : "github-light"
+      };
+
+      var utterances = document.querySelector('iframe');
+      utterances.contentWindow.postMessage(message, 'https://utteranc.es');
+}
+
+let preferDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+window.onload = () => {
+    setCommentsTheme(preferDark.matches);
+}
+
+preferDark.addEventListener("change", (e) => {
+    setCommentsTheme(e.matches)
+})
